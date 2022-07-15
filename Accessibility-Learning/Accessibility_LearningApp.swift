f8503671e9e07e6bc1ce4a19b12570a800ed4034
Takasur Azeem <takasurazeem@gmail.com>
2022-07-15 18:46:51 +0500

    Initial Commit

---
+++ Accessibility-Learning/Accessibility_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_LearningApp.swift
+//  Accessibility-Learning
+//
+//  Created by Takasur Azeem on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessibility-Learning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility-Learning
+//
+//  Created by Takasur Azeem on 15/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
